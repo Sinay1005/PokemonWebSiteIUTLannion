0b3dbeb60e10fee2320dbaeac860eb394a3d5e58
ldesperrois <lucas.desperrois29@gmail.com>
2024-03-12 17:46:49 +0100

    Ajout nom variable
    v 0.3

---
+++ JS/Pokemon.js
@@ -0,0 +1,34 @@
+
+// Classe Pokemon qui définit un Objet Pokemon
+class Pokemon{
+
+    // Objet qui stock les pokemons
+    static  all_pokemons = {};
+
+    // id,name, form : normal,bases : stamina,defense,attack , type pokemon, charged_moved, fast_moves,num generation,
+
+    // Constructeur de la classe Pokemon
+
+    constructor(id,name,forme,basesStamina,basesDefense,attack,pokemonType,chargedMoved,fastMoves, numGeneration){
+        // Identifiant Pokemon
+        this.m_id = id;
+        // Forme du Pokemon
+        this.m_base = forme;
+        // Nom du pokemon
+        this.m_nom = name;
+        // Stamina du Pokemon 
+        this.m_stamina = basesStamina;
+        // defense
+        this.m_defense
+
+    }
+
+
+
+
+
+
+
+
+
+}
